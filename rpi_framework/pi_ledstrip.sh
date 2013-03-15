1224c978a016384cfd6c437bd8f0ca49dba78bf4
taylotr <taylotr@gmail.com>
2013-03-15 14:20:17 -0400

    First quick test script for the pi-blaster, not permanent, just experimenting

--- rpi_framework/pi_ledstrip.sh
+++ rpi_framework/pi_ledstrip.sh
@@ -4,6 +4,6 @@ x=1
 while [ $x -le 5 ]
 do 
 
-	for i in `cat tt.in`; do echo "$i" > /dev/pi-blaster; sleep 0.1; done
+	for i in `cat pi_ledstrip.in`; do echo "$i" > /dev/pi-blaster; sleep 0.1; done
 	x=$(( $x +1))
 done
